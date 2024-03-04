7fc0d3bf8c2419651541359eab2ce21c447d8045
dvtung <122477565+dinh-van-tung@users.noreply.github.com>
2024-03-05 00:22:16 +0700

    add file vayne-and-fence.cpp (Codeforces)
    https://codeforces.com/problemset/problem/677/A

---
+++ vayna-and-fence.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <iomanip> 
+using namespace std;
+#define en '\n';
+typedef long long ll;
+typedef long double ld;
+ll mod = 1e9 + 7;
+
+int main()
+{
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     cout.tie(0);
+
+     //freopen(".INP", "r", stdin);
+     //freopen(".OUT", "w", stdout);
+
+     ll n, h;
+     cin >> n >> h;
+     ll A[n];
+     ll cnt = 0;
+     for (ll i = 0; i < n; i++)
+     {
+          cin >> A[i];
+          if (A[i] <= h)
+               cnt++;
+          else
+               cnt += 2;
+     }
+     cout << cnt;
+     return 0;
+}
